f65d438531dd3b27a8f2d1ec0fc4b69a484240ba
Guillaume Badi <gbadi@student.42.fr>
2016-03-15 18:50:53 +0100

    npm stuff

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Trello from "node-trello"
 import Debug from 'debug'
-import config from '../config.json'
+import config from '../config.dist.json'
 
 const debug = Debug('trello-dispatch')
 debug("config", config)
